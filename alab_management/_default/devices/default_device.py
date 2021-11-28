05d82400de2f683f83c2c17c750a9b2cb1a9d389
Yuxing Fei <36567193+idocx@users.noreply.github.com>
2021-11-28 00:31:04 -0800

    fix(_default): add _default to ignore list for CI

--- alab_management/_default/devices/default_device.py
+++ alab_management/_default/devices/default_device.py
@@ -1,18 +1,22 @@
-import random
-from threading import Timer
 from typing import ClassVar
 
 from alab_management import BaseDevice, SamplePosition, add_device
 
 
 class DefaultDevice(BaseDevice):
+    """
+    Default device definition, refer to https://idocx.github.io/alab_management/device_definition.html
+    """
     description: ClassVar[str] = "Default device"
 
     def __init__(self, *args, **kwargs):
-        super(DefaultDevice, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     @property
     def sample_positions(self):
+        """
+        Sample positions define the sample positions related to this device
+        """
         return [
             SamplePosition(name="DefaultSamplePosition", number=4, description="Default sample position")
         ]
--- alab_management/_default/tasks/default_task.py
+++ alab_management/_default/tasks/default_task.py
@@ -4,11 +4,15 @@ from alab_management.task_view.task import BaseTask
 
 
 class DefaultTask(BaseTask):
+    """
+    The default task, refer to https://idocx.github.io/alab_management/task_definition.html for more details
+    """
+
     def __init__(self, sample: ObjectId, *args, **kwargs):
-        super(DefaultTask, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.sample = sample
 
     def run(self):
         with self.lab_manager.request_resources({None: [{"prefix": "DefaultSamplePosition", "number": 2}]}) \
-                as (devices, sample_positions):
+                as (_, sample_positions):
             self.lab_manager.move_sample(self.sample, sample_positions[None]["DefaultSamplePosition"][0])
