b742137a6704137757f10f31ef0b5ea1c985cb50
Rishi Kumar <rishi42@gmail.com>
2022-06-29 15:14:41 -0700

    pip install broken by import in setup.py Fixes #14

--- setup.py
+++ setup.py
@@ -2,24 +2,27 @@ from pathlib import Path
 
 from setuptools import setup, find_packages
 
-from alab_management import __version__
+# from alab_management import __version__
 
 setup(
     name="alab_management",
     packages=find_packages(exclude=["tests", "tests.*"]),
     package_data={"alab_management": ["py.typed"]},
-    version=__version__,
+    version="0.4.1",
     author="Alab Project Team",
     python_requires=">=3.6",
     description="Workflow management system for alab",
     zip_safe=False,
     install_requires=[
         package.strip("\n")
-        for package in (Path(__file__).parent / "requirements.txt").open("r", encoding="utf-8").readlines()],
+        for package in (Path(__file__).parent / "requirements.txt")
+        .open("r", encoding="utf-8")
+        .readlines()
+    ],
     include_package_data=True,
     entry_points={
         "console_scripts": [
             "alabos = alab_management.scripts.cli:cli",
         ]
-    }
+    },
 )
