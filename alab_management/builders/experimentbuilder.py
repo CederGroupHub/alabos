69c1a720463d9b3923252cc7e21bdb922ae7eef2
rekumar <rekumar@lbl.gov>
2023-08-30 11:03:54 -1000

    alabos sample/exp builders

--- alab_management/builders/experimentbuilder.py
+++ alab_management/builders/experimentbuilder.py
@@ -4,145 +4,225 @@ from .samplebuilder import SampleBuilder
 import matplotlib.pyplot as plt
 
 
+from typing import Optional, List
+from labgraph import Material
+from labgraph.utils.plot import plot_multiple_samples
+from bson import ObjectId
+
+
 class ExperimentBuilder:
-    """
-    It takes a list of samples and a list of tasks, and returns a dictionary
-    that can be used to generate an input file for the `experiment` command
-
-    Args:
-      name (str): The name of the experiment.
-    """
-
-    def __init__(self, name: str, tags: Optional[List[str]] = None, **metadata):
-        """
-        Args:
-          name (str): The name of the experiment.
-        """
+    def __init__(self, name: str, tags: Optional[List[str]] = None, **contents):
         self.name = name
-        self._samples: List[SampleBuilder] = []
-        self._tasks: Dict[str, Dict[str, Any]] = {}
         self.tags = tags or []
-        self.metadata = metadata
+        self.contents = contents
+        self.samples = []
+        self._id = ObjectId()
 
-    def add_sample(
-        self, name: str, tags: Optional[List[str]] = None, **metadata
-    ) -> SampleBuilder:
-        """
-        This function adds a sample to the experiment
+    def add_sample(self, sample: SampleBuilder):
+        self.samples.append(sample)
 
-        Args:
-          name (str): The name of the sample. This must be unique within this ExperimentBuilder.
-          **metadata: Any additional keyword arguments will be attached to this sample as metadata.
-        Returns:
-          A SampleBuilder object. This can be used to add tasks to the sample.
-        """
-        if any(name == sample.name for sample in self._samples):
-            raise ValueError(f"Sample by name {name} already exists.")
-        sample = SampleBuilder(name, experiment=self, tags=tags, **metadata)
+    def plot(self, ax=None):
+        return plot_multiple_samples(self.samples, ax=ax)
 
-        # TODO ensure that the metadata is json/bson serializable
-        self._samples.append(sample)
-        return sample
-
-    def add_task(
-        self,
-        task_id: str,
-        task_name: str,
-        task_kwargs: Dict[str, Any],
-        samples: List[SampleBuilder],
-    ) -> None:
-        if task_id in self._tasks:
-            return
-        self._tasks[task_id] = {
-            "type": task_name,
-            "parameters": task_kwargs,
-            "samples": [sample.name for sample in samples],
-        }
-
-    def to_dict(self):
+    def to_dict(self) -> dict:
         samples = []
         tasks = []
         task_ids = {}
 
-        for sample in self._samples:
-            samples.append(sample.to_dict())
+        for sample in self.samples:
+            # put into original ALabOS format
+            sdict = sample.to_dict()
+            sdict.pop("nodes")
+            sdict["metadata"] = sdict.pop("contents", {})
+            sdict["metadata"]["description"] = sdict.pop("description", "")
+            sdict["sample_id"] = str(sdict.pop("_id"))
+            samples.append(sdict)
+
             last_task_id = None
-            for task_id in sample.tasks:
-                task = self._tasks[task_id]
-                task["_id"] = str(task_id)
-                if task_id not in task_ids:
-                    task_ids[task_id] = len(tasks)
-                    # task["next_tasks"] = set()
-                    task["prev_tasks"] = set()
-                    tasks.append(task)
+            for node in sample.nodes:
+                if isinstance(node, Material):
+                    continue  # only add task nodes (Action, Measurement, Analysis)
+
+                # put into original ALabOS format
+                task_entry = node.to_dict()
+                task_entry["task_id"] = str(task_entry.pop("_id"))
+                task_entry["type"] = task_entry.pop("name")
+                task_entry["parameters"] = task_entry["contents"].get("parameters", {})
+                task_entry = {
+                    k: task_entry[k] for k in ["task_id", "type", "parameters"]
+                }
+                task_entry["samples"] = []
+                task_entry["labgraph_node_type"] = node.labgraph_type
+
+                task_entry["prev_tasks"] = set()
+                if node.id not in task_ids:
+                    task_ids[node.id] = len(tasks)
+
+                    for upstream in node.upstream:
+                        if upstream["node_type"] == "Material":
+                            continue
+                        task_entry["prev_tasks"].add(task_ids[upstream["node_id"]])
+
+                    tasks.append(task_entry)
+
+                tasks[task_ids[node.id]]["samples"].append(sample.name)
                 if last_task_id is not None:
                     # tasks[task_ids[last_task_id]]["next_tasks"].add(task_ids[task_id])
-                    task["prev_tasks"].add(task_ids[last_task_id])
-                last_task_id = task_id
+                    task_entry["prev_tasks"].add(task_ids[last_task_id])
+                last_task_id = node.id
 
         for task in tasks:
-            # task["next_tasks"] = list(task["next_tasks"])
             task["prev_tasks"] = list(task["prev_tasks"])
 
         return {
             "name": self.name,
             "tags": self.tags,
-            "metadata": self.metadata,
+            "metadata": self.contents,
             "samples": samples,
             "tasks": tasks,
+            "_id": self._id,
         }
 
-    def generate_input_file(
-        self, filename: str, fmt: Literal["json", "yaml"] = "json"
-    ) -> None:
-        with Path(filename).open("w", encoding="utf-8") as f:
-            if fmt == "json":
-                import json
 
-                json.dump(self.to_dict(), f, indent=2)
-            elif fmt == "yaml":
-                import yaml
+# class ExperimentBuilder:
+#     """
+#     It takes a list of samples and a list of tasks, and returns a dictionary
+#     that can be used to generate an input file for the `experiment` command
 
-                yaml.dump(self.to_dict(), f, default_flow_style=False, indent=2)
+#     Args:
+#       name (str): The name of the experiment.
+#     """
 
-    def plot(self, ax=None):
-        import networkx as nx
-        import matplotlib.pyplot as plt
+#     def __init__(self, name: str, tags: Optional[List[str]] = None, **metadata):
+#         """
+#         Args:
+#           name (str): The name of the experiment.
+#         """
+#         self.name = name
+#         self._samples: List[SampleBuilder] = []
+#         self._tasks: Dict[str, Dict[str, Any]] = {}
+#         self.tags = tags or []
+#         self.metadata = metadata
 
-        if ax is None:
-            _, ax = plt.subplots(figsize=(8, 6))
+#     def add_sample(
+#         self, name: str, tags: Optional[List[str]] = None, **metadata
+#     ) -> SampleBuilder:
+#         """
+#         This function adds a sample to the experiment
 
-        task_list = self.to_dict()["tasks"]
+#         Args:
+#           name (str): The name of the sample. This must be unique within this ExperimentBuilder.
+#           **metadata: Any additional keyword arguments will be attached to this sample as metadata.
+#         Returns:
+#           A SampleBuilder object. This can be used to add tasks to the sample.
+#         """
+#         if any(name == sample.name for sample in self._samples):
+#             raise ValueError(f"Sample by name {name} already exists.")
+#         sample = SampleBuilder(name, experiment=self, tags=tags, **metadata)
 
-        unique_tasks = set([task["type"] for task in task_list])
-        color_key = {
-            nodetype: plt.cm.tab10(i) for i, nodetype in enumerate(unique_tasks)
-        }
-        node_colors = []
-        node_labels = {}
-        for task in task_list:
-            node_colors.append(color_key[task["type"]])
-            node_labels[task["_id"]] = f"{task['type']} ({len(task['samples'])})"
+#         # TODO ensure that the metadata is json/bson serializable
+#         self._samples.append(sample)
+#         return sample
 
-        g = nx.DiGraph()
-        for task in task_list:
-            g.add_node(task["_id"], name=task["type"], samples=len(task["samples"]))
-            for prev in task["prev_tasks"]:
-                g.add_edge(task_list[prev]["_id"], task["_id"])
+#     def add_task(
+#         self,
+#         task_id: str,
+#         task_name: str,
+#         task_kwargs: Dict[str, Any],
+#         samples: List[SampleBuilder],
+#     ) -> None:
+#         if task_id in self._tasks:
+#             return
+#         self._tasks[task_id] = {
+#             "type": task_name,
+#             "parameters": task_kwargs,
+#             "samples": [sample.name for sample in samples],
+#         }
 
-        try:
-            pos = nx.nx_agraph.graphviz_layout(g, prog="dot")
-        except:
-            pos = nx.spring_layout(g)
+#     def to_dict(self):
+#         samples = []
+#         tasks = []
+#         task_ids = {}
 
-        nx.draw(
-            g,
-            with_labels=True,
-            node_color=node_colors,
-            labels=node_labels,
-            pos=pos,
-            ax=ax,
-        )
+#         for sample in self._samples:
+#             samples.append(sample.to_dict())
+#             last_task_id = None
+#             for task_id in sample.tasks:
+#                 task = self._tasks[task_id]
+#                 task["_id"] = str(task_id)
+#                 if task_id not in task_ids:
+#                     task_ids[task_id] = len(tasks)
+#                     # task["next_tasks"] = set()
+#                     task["prev_tasks"] = set()
+#                     tasks.append(task)
+#                 if last_task_id is not None:
+#                     # tasks[task_ids[last_task_id]]["next_tasks"].add(task_ids[task_id])
+#                     task["prev_tasks"].add(task_ids[last_task_id])
+#                 last_task_id = task_id
 
-    def __repr__(self):
-        return f"<ExperimentBuilder: {self.name}>"
+#         for task in tasks:
+#             # task["next_tasks"] = list(task["next_tasks"])
+#             task["prev_tasks"] = list(task["prev_tasks"])
+
+#         return {
+#             "name": self.name,
+#             "tags": self.tags,
+#             "metadata": self.metadata,
+#             "samples": samples,
+#             "tasks": tasks,
+#         }
+
+#     def generate_input_file(
+#         self, filename: str, fmt: Literal["json", "yaml"] = "json"
+#     ) -> None:
+#         with Path(filename).open("w", encoding="utf-8") as f:
+#             if fmt == "json":
+#                 import json
+
+#                 json.dump(self.to_dict(), f, indent=2)
+#             elif fmt == "yaml":
+#                 import yaml
+
+#                 yaml.dump(self.to_dict(), f, default_flow_style=False, indent=2)
+
+#     def plot(self, ax=None):
+#         import networkx as nx
+#         import matplotlib.pyplot as plt
+
+#         if ax is None:
+#             _, ax = plt.subplots(figsize=(8, 6))
+
+#         task_list = self.to_dict()["tasks"]
+
+#         unique_tasks = set([task["type"] for task in task_list])
+#         color_key = {
+#             nodetype: plt.cm.tab10(i) for i, nodetype in enumerate(unique_tasks)
+#         }
+#         node_colors = []
+#         node_labels = {}
+#         for task in task_list:
+#             node_colors.append(color_key[task["type"]])
+#             node_labels[task["_id"]] = f"{task['type']} ({len(task['samples'])})"
+
+#         g = nx.DiGraph()
+#         for task in task_list:
+#             g.add_node(task["_id"], name=task["type"], samples=len(task["samples"]))
+#             for prev in task["prev_tasks"]:
+#                 g.add_edge(task_list[prev]["_id"], task["_id"])
+
+#         try:
+#             pos = nx.nx_agraph.graphviz_layout(g, prog="dot")
+#         except:
+#             pos = nx.spring_layout(g)
+
+#         nx.draw(
+#             g,
+#             with_labels=True,
+#             node_color=node_colors,
+#             labels=node_labels,
+#             pos=pos,
+#             ax=ax,
+#         )
+
+#     def __repr__(self):
+#         return f"<ExperimentBuilder: {self.name}>"
--- alab_management/builders/samplebuilder.py
+++ alab_management/builders/samplebuilder.py
@@ -12,57 +12,118 @@ if TYPE_CHECKING:
     from alab_management.builders.experimentbuilder import ExperimentBuilder
 
 
-class SampleBuilder:
-    def __init__(
-        self,
-        name: str,
-        experiment: "ExperimentBuilder",
-        tags: Optional[List[str]] = None,
-        **metadata,
-    ):
-        self.name = name
-        self._tasks: List[str] = []  # type: ignore
-        self.experiment = experiment
-        self.metadata = metadata
-        self._id = str(ObjectId())
-        self.tags = tags or []
+from alab_management import BaseTask, BaseAction, BaseAnalysis, BaseMeasurement
+from labgraph import Sample as LabgraphSample, Material, Measurement, Analysis,
+from labgraph.data.nodes import UnspecifiedAmountIngredient
 
-    def add_task(
-        self,
-        task_id: str,
-    ) -> None:
-        if task_id not in self._tasks:
-            self._tasks.append(task_id)
+from typing import List
 
-    def to_dict(self) -> Dict[str, Any]:
-        """Return Sample as a dictionary. This looks like:
+class SampleBuilder(LabgraphSample):
+    def __init__(self, name:str, *args, **kwargs):
+        self.current_material = None
+        super().__init__(name=name, *args, **kwargs)
 
-        {
-            "_id": str(ObjectId),
-            "name": str,
-            "tags": List[str],
-            "metadata": Dict[str, Any],
-        }
+    def add_action(self, task: BaseAction, input_materials: List[Material] = None, generated_materials: List[Material] = None):
+        if input_materials:
+            for material in input_materials:
+                task.add_ingredient(UnspecifiedAmountIngredient(material))
+        elif self.current_material:
+            task.add_ingredient(UnspecifiedAmountIngredient(material = self.current_material))
 
-        Returns:
-            Dict[str, Any]: sample as a dictionary
-        """
-        return {
-            "_id": str(self._id),
-            "name": self.name,
-            "tags": self.tags,
-            "metadata": self.metadata,
-        }
+        if generated_materials:
+            for material in generated_materials:
+                task.add_generated_material(material)
+                self.add_node(material)
+            self.current_material = generated_materials[0]
+        else:
+            generated_material = task.make_generic_generated_material()
+            self.add_node(generated_material)
+            self.current_material = generated_material
 
-    @property
-    def tasks(self):
-        return self._tasks
+        self.add_node(task)
+        return task
 
-    def __eq__(self, other):
-        return self._id == other._id
 
-    def __repr__(self):
-        return f"<Sample: {self.name}>"
+    def add_measurement(self, task: BaseMeasurement, input_material: Material = None):
+        material = None
+        
+        if input_material:
+            if input_material not in self.nodes:
+                raise ValueError(f"The input material must be a node in the labgraph sample! We do not yet contain ({input_material})")
+            material = input_material
+        elif self.current_material:
+            material = self.current_material
+        else:
+            raise ValueError("No material has been generated in this sample yet. To add a measurement, you either specify the input material or first add an action that generates a material!")
+
+        task.material = material
+        self.add_node(task)
+        return task
+
+    def add_analysis(self, task: BaseAnalysis, input_measurements: List[Measurement] = None, input_analyses: List[Analysis] = None):
+        if not input_measurements and not input_analyses:
+            raise ValueError("You must specify either input measurements or input analyses!")
+        
+        for measurement in input_measurements or []:
+            task.add_measurement(measurement)
+        for analysis in input_analyses or []:
+            task.add_upstream_analysis(analysis)
+
+        self.add_node(task)
+
+        return task
+
+# class SampleBuilder:
+#     def __init__(
+#         self,
+#         name: str,
+#         experiment: "ExperimentBuilder",
+#         tags: Optional[List[str]] = None,
+#         **metadata,
+#     ):
+#         self.name = name
+#         self._tasks: List[str] = []  # type: ignore
+#         self.experiment = experiment
+#         self.metadata = metadata
+#         self._id = str(ObjectId())
+#         self.tags = tags or []
+
+#     def add_task(
+#         self,
+#         task_id: str,
+#     ) -> None:
+#         if task_id not in self._tasks:
+#             self._tasks.append(task_id)
+
+#     def to_dict(self) -> Dict[str, Any]:
+#         """Return Sample as a dictionary. This looks like:
+
+#         {
+#             "_id": str(ObjectId),
+#             "name": str,
+#             "tags": List[str],
+#             "metadata": Dict[str, Any],
+#         }
+
+#         Returns:
+#             Dict[str, Any]: sample as a dictionary
+#         """
+#         return {
+#             "_id": str(self._id),
+#             "name": self.name,
+#             "tags": self.tags,
+#             "metadata": self.metadata,
+#         }
+
+#     @property
+#     def tasks(self):
+#         return self._tasks
+
+#     def __eq__(self, other):
+#         return self._id == other._id
+
+#     def __repr__(self):
+#         return f"<Sample: {self.name}>"
 
 
 ### Format checking for API inputs
