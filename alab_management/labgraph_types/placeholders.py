d62dd1fabc619a75e2d3632c42353f1ab30f37b6
Rishi Kumar <re.kumar@icloud.com>
2023-09-05 23:52:13 -1000

    previous

--- alab_management/labgraph_types/placeholders.py
+++ alab_management/labgraph_types/placeholders.py
@@ -1,11 +1,14 @@
-from labgraph import Actor
+from labgraph import Actor, ActorView
+from alab_management.utils.data_objects import get_labgraph_mongodb
 
 
+actor_view = ActorView(labgraph_mongodb_instance=get_labgraph_mongodb())
 try:
-    placeholder_actor = Actor.get_by_name("Placeholder before execution")
+    placeholder_actor = actor_view.get_by_name("Placeholder before execution")[0]
 except:
     placeholder_actor = Actor(
         name="Placeholder before execution",
+        tags=["ALabOS"],
         description="This is a placeholder actor assigned to tasks that are planned but have not yet been executed. This actor will be replaced by the actual actor when the task is executed.",
     )
-    placeholder_actor.save()
+    actor_view.add(placeholder_actor)
