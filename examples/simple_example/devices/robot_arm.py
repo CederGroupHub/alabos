e1480c64f2b99e5db0664bf9bf1e739d8e550051
Hrushikesh Sahasrabuddhe <hrushikesh_s@berkeley.edu>
2023-09-13 16:26:30 -0700

    Adding the support of pre-commit, flake8, mypy, coverage report, ruff linter and black -- motivation taken from jobflow package developed by Alex Ganose

--- examples/simple_example/devices/robot_arm.py
+++ examples/simple_example/devices/robot_arm.py
@@ -1,36 +1,50 @@
-from typing import ClassVar
-import time
+"""A robot arm that can move the sample holder."""
 
-from alab_control.robot_arm_ur5e import BaseURRobot
+import time
+from typing import ClassVar
+
+from alab_control.robot_arm_ur5e import URRobotSecondary
 
 from alab_management import BaseDevice, SamplePosition
 
 
 class RobotArm(BaseDevice):
+    """A robot arm that can move the sample holder."""
+
     description: ClassVar[str] = "UR5e robot arm"
 
     def __init__(self, address: str, port: int = 29999, *args, **kwargs):
-        super(RobotArm, self).__init__(*args, **kwargs)
+        """Initialize the robot arm.
+
+        Args:
+            address: the ip address of the robot arm
+            port: the port of the robot arm.
+        """
+        super().__init__(*args, **kwargs)
         self.address = address
         self.port = port
-        self.driver = URRobot(self.address, port=port)
+        self.driver = URRobotSecondary(self.address, port=port)
 
     @property
     def sample_positions(self):
+        """The sample positions that the robot arm can move to."""
         return [
             SamplePosition(
-                "{name}/sample_holder".format(name=self.name),
-                description="The position that can hold the sample"
+                f"{self.name}/sample_holder",
+                description="The position that can hold the sample",
             ),
         ]
 
     def emergent_stop(self):
+        """Stop the robot arm."""
         self.driver.stop()
 
     def run_program(self, program):
+        """Run a program on the robot arm."""
         self.driver.run_program(program)
         time.sleep(2)
         self.driver.wait_for_finish()
 
     def is_running(self):
+        """Check if the robot arm is running."""
         return self.driver.is_running()
