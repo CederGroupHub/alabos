af2f392e4e406c69997f8d7fe02bb32a25a3223e
Hrushikesh Sahasrabuddhe <hrushikesh_s@berkeley.edu>
2023-10-03 10:50:32 -0700

    Update enums.py

--- alab_management/task_manager/enums.py
+++ alab_management/task_manager/enums.py
@@ -4,9 +4,7 @@ _EXTRA_REQUEST: str = "__nodevice"
 
 
 class RequestStatus(Enum):
-    """
-    The status for a request. It will be stored in the database
-    """
+    """The status for a request. It will be stored in the database."""
 
     PENDING = auto()
     FULFILLED = auto()
