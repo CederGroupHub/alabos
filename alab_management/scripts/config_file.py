eab2adb46c0b29c0c56d6216966a350e7a29bfa1
Hrushikesh Sahasrabuddhe <hrushikesh_s@berkeley.edu>
2023-12-04 09:53:03 -0800

    Update config_file.py

--- alab_management/scripts/config_file.py
+++ alab_management/scripts/config_file.py
@@ -2,7 +2,4 @@ def config_file_update(sim_mode: bool = False):
     """Update the config file."""
     from alab_management.config import AlabConfig
 
-    print(f"config_file_update: sim_mode = {sim_mode}")
-    # AlabConfig(sim_mode)
     config = AlabConfig(sim_mode=sim_mode)
-    print(f"config = {config}")
