f3dfc544b88ac3bf869ba26115acddb54342b58e
Yuxing Fei <yuxingfei@berkeley.edu>
2024-10-08 09:14:07 -0700

    fix(get-exp-result-import): fix the import of `get_experiment_result` at the top of the package and change the function name in the doc from `get_experiment_results` to `get_experiment_result`

--- alab_management/builders/__init__.py
+++ alab_management/builders/__init__.py
@@ -3,6 +3,10 @@ Builders are used to create the experiment and sample objects. They are
 used to create the input file for the `experiment` command.
 """
 
-from .experimentbuilder import ExperimentBuilder, get_experiment_status
+from .experimentbuilder import (
+    ExperimentBuilder,
+    get_experiment_result,
+    get_experiment_status,
+)
 from .samplebuilder import SampleBuilder
 from .utils import append_task
