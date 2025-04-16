20f8d950fc2325921a0bdd201113178030b9aa5d
Yuxing Fei <yuxingfei@berkeley.edu>
2025-04-16 11:11:28 -0700

    feat(reload): add reload function

--- alab_management/utils/module_ops.py
+++ alab_management/utils/module_ops.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from types import ModuleType
 
 
-def import_module_from_path(path: str | Path) -> ModuleType:
+def import_module_from_path(path: str | Path, reload: bool = False) -> ModuleType:
     """Import a module by its path. If it is a subpackage, keep going back and import the parent package."""
     path = Path(path).resolve()
     dir_path = path.parent if path.is_file() else path
@@ -25,11 +25,15 @@ def import_module_from_path(path: str | Path) -> ModuleType:
     # Determine module name by relative path from top-level package
     rel_path = path.relative_to(dir_path.parent)
     module_name = ".".join(rel_path.with_suffix("").parts)
-    # Import the module
-    return importlib.import_module(module_name, module_name)
+
+    # Import (or reload) the module
+    if module_name in sys.modules and reload:
+        return importlib.reload(sys.modules[module_name])
+    else:
+        return importlib.import_module(module_name)
 
 
-def load_definition():
+def load_definition(reload: bool = False) -> None:
     """Load device and task definitions from file (specified in config file)."""
     from alab_management.config import AlabOSConfig
 
@@ -43,4 +47,4 @@ def load_definition():
         else config.path.parent / dir_to_import_from
     )
 
-    import_module_from_path(dir_to_import_from)
+    import_module_from_path(dir_to_import_from, reload=reload)
