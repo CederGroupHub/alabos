7bf77d34169d68ae1146c989b4f12fcd9b64f181
Yuxing Fei <yuxingfei@berkeley.edu>
2025-06-04 22:38:46 -0700

    Refactor imports in task_actor.py for improved organization and clarity

--- alab_management/task_actor.py
+++ alab_management/task_actor.py
@@ -8,6 +8,10 @@ import logging
 from traceback import format_exc
 
 import dramatiq
+from bson import ObjectId
+from dramatiq_abort import Abort
+from pydantic import BaseModel, ValidationError
+
 from alab_management.logger import DBLogger
 from alab_management.sample_view import SampleView
 from alab_management.task_view import BaseTask, TaskStatus, TaskView
@@ -15,9 +19,6 @@ from alab_management.utils.data_objects import get_rabbitmq_broker
 from alab_management.utils.logger import set_up_rich_handler
 from alab_management.utils.middleware import register_abortable_middleware
 from alab_management.utils.module_ops import load_definition
-from bson import ObjectId
-from dramatiq_abort import Abort
-from pydantic import BaseModel, ValidationError
 
 dramatiq.set_broker(get_rabbitmq_broker())
 
